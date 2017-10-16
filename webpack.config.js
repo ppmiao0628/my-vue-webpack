63d37bef7256c5c56d4a9c7af7819882733dcdfc
mpp <ppmiao0628@outlook.com>
2017-10-16 20:49:34 +0800

    init wabpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require("webpack");
+
+module.exports = {
+    entry: {
+        bundle: ["./src/app.js"]
+    },
+    output: {
+        path: __dirname,
+        publicPath: "/",
+        filename: 'dist/[name].js'
+    },
+    module: {
+        loaders: [
+            {test: /\.html$/, loaders: ['html']},
+            {test: /(\.js)$/, loaders: ["babel"], exclude: /node_modules/, query: {presets: ["es2015"]}}
+        ]
+    },
+    resolve: {},
+    plugins: [
+        /*
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warning: false
+            }
+        })
+        */
+    ]
+}
